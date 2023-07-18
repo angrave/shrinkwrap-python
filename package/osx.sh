83657e39c48be621402d4e2110b703ca6507c835
Lawrence Angrave <4468456+angrave@users.noreply.github.com>
2023-07-18 14:50:17 -0500

    Create osx.sh

---
+++ package/osx.sh
@@ -0,0 +1,13 @@
+# pyinstaller uses Pillow to convert png into platform-specific icon format
+# argv-emulation is osx specific
+pyinstaller \
+   --clean \
+   --windowed  \
+   --noconfirm \
+   --argv-emulation \
+   --paths ./venv/lib/python3.11/site-packages \
+   --icon ./Koko.png \
+   --name Koko
+   src/main.py
+
+dmgbuild -s package/osxdmg.py "Koko" koko.dmg
