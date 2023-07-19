045fb7e610f7ab0a8c49cc2fd39c0e7c477af849
Lawrence Angrave <4468456+angrave@users.noreply.github.com>
2023-07-19 16:30:55 -0500

    Update osx.sh

--- package/osx.sh
+++ package/osx.sh
@@ -1,11 +1,20 @@
 # pyinstaller uses Pillow to convert png into platform-specific icon format
 # argv-emulation is osx specific
+# The pyinstaller line below could be converted into python code 
+# (which could also have conditional os-specific options).
+# see https://pyinstaller.org/en/stable/usage.html#running-pyinstaller-from-python-code
+
+# Code signing-
+# https://pyinstaller.org/en/stable/usage.html
+# 
+
 pyinstaller \
    --clean \
    --windowed  \
    --noconfirm \
    --argv-emulation \
    --paths ./venv/lib/python3.11/site-packages \
+   --add-data=/src/Saint_Helens.png:.
    --icon ./Saint_Helens.png \
    --name Saint_Helens \
    src/main.py
