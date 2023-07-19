0159a8da55a39e0d6e9964598e645d38c0ad856a
Lawrence Angrave <4468456+angrave@users.noreply.github.com>
2023-07-18 23:46:40 -0500

    Update win.sh

--- package/win.sh
+++ package/win.sh
@@ -1,2 +1,19 @@
+# Bash script
+
 mkdir dist
-echo hello > dist/Saint_Hellens_install.exe
+# echo hello > dist/Saint_Helens/Saint_Helens_install.exe
+
+# pyinstaller uses Pillow to convert png into platform-specific icon format
+# argv-emulation is osx specific
+pyinstaller \
+   --clean \
+   --windowed  \
+   --noconfirm \ 
+   --paths ./venv/lib/python3.11/site-packages \
+   --icon ./Saint_Helens.png \
+   --name Saint_Helens \
+   src/main.py
+
+ls -R dist/
+
+
