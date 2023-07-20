13f0e40668fe206fbe1c8aec7951a2aa129e1a4c
Lawrence Angrave <4468456+angrave@users.noreply.github.com>
2023-07-20 10:29:58 -0500

    Update win.sh

--- package/win.sh
+++ package/win.sh
@@ -3,6 +3,10 @@
 mkdir dist
 # echo hello > dist/Saint_Helens/Saint_Helens_install.exe
 
+# Lightly edited version of older osx.sh pyinstaller.
+# Todo: Combine these into a pure python option that supports platform-specific options
+
+
 # pyinstaller uses Pillow to convert png into platform-specific icon format
 # argv-emulation is osx specific
 pyinstaller \
